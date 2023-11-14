d47de2dd1371a277355d74ab1fe0b9b055da974a
Twocram <2002maslovartem@gmail.com>
2023-11-14 19:01:58 +0500

    [feat] upd alias for project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: "./src",
+      },
+      {
+        find: "@components",
+        replacement: "./src/components",
+      },
+      {
+        find: "@assets",
+        replacement: "./src/assets",
+      },
+    ],
+  },
+});
