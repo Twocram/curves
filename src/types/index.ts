0c182b992efe736c1776e771abca0de020500c38
Twocram <2002maslovartem@gmail.com>
2023-11-15 13:05:54 +0500

    [feat] add types

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type TButtonProps = {
+  isDisabled: boolean;
+  type: string;
+  size: string;
+};
+
+export type TLinkButtonProps = {
+  label: string;
+  url: string;
+};
+
+export type TCardInfoProps = {
+  caption: string;
+  date: string;
+  size: string;
+  duration: string;
+  type: string;
+};
+
+export type TCardAboutProps = {
+  desc: string;
+};
