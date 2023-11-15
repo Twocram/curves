762073f10392f7e7e86bb4fd8aab67b5a99149e8
Twocram <2002maslovartem@gmail.com>
2023-11-16 00:15:00 +0500

    [fix] durationc converter

--- src/utils/converter/index.ts
+++ src/utils/converter/index.ts
@@ -39,7 +39,15 @@ export const durationConverter = (duration: number): string => {
   const minutes: number = Math.floor((duration - hours * 3600) / 60);
   const seconds: number = Math.floor(duration - hours * 3600 - minutes * 60);
 
-  return `${hours < 10 ? "0" + hours : hours}:${
-    minutes < 10 ? "0" + minutes : minutes
-  }:${seconds < 10 ? "0" + seconds : seconds}`;
+  let formattedDuration = "";
+
+  if (hours > 0) {
+    formattedDuration += `${hours < 10 ? "0" + hours : hours}:`;
+  }
+
+  formattedDuration += `${minutes < 10 ? "0" + minutes : minutes}:${
+    seconds < 10 ? "0" + seconds : seconds
+  }`;
+
+  return formattedDuration;
 };
