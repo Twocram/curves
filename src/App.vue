1b85420b2c2c16cf9bf3d5c22e2976aa7b6d9722
Twocram <2002maslovartem@gmail.com>
2023-11-15 20:42:06 +0500

    [feat] basic template for main page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,48 @@
 <script setup lang="ts">
-import VLayout from "@components/v-layout.vue";
+import { onMounted } from "vue";
+import VLayout from "@components/VLayout.vue";
+import VLinkButton from "@components/ui/VLinkButton.vue";
+import VCardInfo from "@components/card/Info.vue";
+import VCardAbout from "@components/card/About.vue";
+
+const apiUrl: string = import.meta.env.VITE_SHOPOT_API_URL;
+
+const getData = async () => {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+onMounted(() => {
+  getData();
+});
 </script>
 
 <template>
   <v-layout>
-    <div class="container"></div>
+    <section class="container">
+      <v-link-button :label="'На главную'" :url="'/'" />
+
+      <div class="content">
+        <div class="content-left">
+          <v-card-info />
+
+          <v-card-about />
+        </div>
+
+        <div class="content-right">
+
+        </div>
+      </div>
+    </section>
   </v-layout>
 </template>
 
@@ -13,5 +51,12 @@ import VLayout from "@components/v-layout.vue";
   max-width: 1240px;
   width: 100%;
   margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+}
+
+.content {
+  position: relative;
 }
 </style>
